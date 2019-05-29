8f17646dad280a49dd36d8a27e2eeddfcb5f4654
xiao.xinmiao <xiaoxm_001@163.com>
2019-05-29 10:40:30 +0800

    #13 add mysql folder into test_info

--- test_info/mysql/table.sql
+++ test_info/mysql/table.sql
@@ -1,6 +1,6 @@
 -- --------------------------------------------------------
--- 主机:                           10.173.208.142
--- 服务器版本:                        5.7.25-log - MySQL Community Server (GPL)
+-- 主机:                           127.0.0.1
+-- 服务器版本:                        5.7.22 - MySQL Community Server (GPL)
 -- 服务器操作系统:                      Linux
 -- HeidiSQL 版本:                  9.5.0.5295
 -- --------------------------------------------------------
@@ -17,6 +17,7 @@ CREATE DATABASE IF NOT EXISTS `fruit` /*!40100 DEFAULT CHARACTER SET utf8 COLLAT
 USE `fruit`;
 
 -- 导出  表 fruit.fruit 结构
+DROP TABLE IF EXISTS `fruit`;
 CREATE TABLE IF NOT EXISTS `fruit` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `created_at` datetime DEFAULT NULL,
@@ -26,17 +27,17 @@ CREATE TABLE IF NOT EXISTS `fruit` (
   `color` varchar(255) DEFAULT NULL,
   `price` bigint(20) DEFAULT NULL,
   `store_code` varchar(255) DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL,
   `unique_id` bigint(20) DEFAULT NULL,
+  `sid` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=96 DEFAULT CHARSET=utf8;
-
-TRUNCATE `fruit`;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- 正在导出表  fruit.fruit 的数据：~2 rows (大约)
 /*!40000 ALTER TABLE `fruit` DISABLE KEYS */;
 INSERT INTO `fruit` (`id`, `created_at`, `updated_at`, `code`, `name`, `color`, `price`, `store_code`, `unique_id`) VALUES
 	(1, '2018-04-15 08:48:59', '2018-10-22 06:00:09', 'apple', 'apple', 'red', 11, NULL, 7707551062674327034),
-    (2, '2018-04-15 08:48:59', '2018-10-22 06:00:09', 'banana', 'banana', 'yellow', 14, NULL, 7707551062674327035);
+	(2, '2018-04-15 08:48:59', '2018-10-22 06:00:09', 'banana', 'banana', 'yellow', 14, NULL, 7707551062674327035);
 /*!40000 ALTER TABLE `fruit` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
