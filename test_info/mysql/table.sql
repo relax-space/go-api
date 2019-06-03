8d4dd48dda8e17152eda0822e025cce1da3f1dd6
xiao.xinmiao <xiaoxm_001@163.com>
2019-06-03 08:39:52 +0800

    #14 test_info

--- test_info/mysql/table.sql
+++ test_info/mysql/table.sql
@@ -17,27 +17,13 @@ CREATE DATABASE IF NOT EXISTS `fruit` /*!40100 DEFAULT CHARACTER SET utf8 COLLAT
 USE `fruit`;
 
 -- 导出  表 fruit.fruit 结构
-DROP TABLE IF EXISTS `fruit`;
-CREATE TABLE IF NOT EXISTS `fruit` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `created_at` datetime DEFAULT NULL,
-  `updated_at` datetime DEFAULT NULL,
-  `code` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `color` varchar(255) DEFAULT NULL,
-  `price` bigint(20) DEFAULT NULL,
-  `store_code` varchar(255) DEFAULT NULL,
-  `deleted_at` datetime DEFAULT NULL,
-  `unique_id` bigint(20) DEFAULT NULL,
-  `sid` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+TRUNCATE TABLE fruit;
 
 -- 正在导出表  fruit.fruit 的数据：~2 rows (大约)
 /*!40000 ALTER TABLE `fruit` DISABLE KEYS */;
-INSERT INTO `fruit` (`id`, `created_at`, `updated_at`, `code`, `name`, `color`, `price`, `store_code`, `unique_id`) VALUES
-	(1, '2018-04-15 08:48:59', '2018-10-22 06:00:09', 'apple', 'apple', 'red', 11, NULL, 7707551062674327034),
-	(2, '2018-04-15 08:48:59', '2018-10-22 06:00:09', 'banana', 'banana', 'yellow', 14, NULL, 7707551062674327035);
+INSERT INTO `fruit` (`id`, `created_at`, `updated_at`, `code`, `name`, `color`, `price`, `store_code`) VALUES
+	(1, '2018-04-15 08:48:59', '2018-10-22 06:00:09', 'apple', 'apple', 'red', 11, NULL),
+	(2, '2018-04-15 08:48:59', '2018-10-22 06:00:09', 'banana', 'banana', 'yellow', 14, NULL);
 /*!40000 ALTER TABLE `fruit` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
